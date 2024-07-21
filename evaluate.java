7b34c878b979cbf5a5cbabadbbffa5b0dbb54b0f
nagaraja6050266 <nagaraja6050266@gmail.com>
2024-07-21 20:27:30 +0530

    Added gitignore

---
+++ evaluate.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class evaluate{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the expression: ");
+        String expression=sc.nextLine();
+        int[] arr=expression.toArray();
+        
+
+    }
+    
+    public float bracket(int[] arr){
+        
+    }
+}
