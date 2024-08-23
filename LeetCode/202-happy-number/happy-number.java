c0cfa1d94850580ec19dbc1a60493d1602a8114b
Nagaraja <98215614+nagaraja6050266@users.noreply.github.com>
2024-08-23 12:12:00 +0530

    Time: 16 ms (5.14%) | Memory: 41.3 MB (5.69%) - LeetSync

---
+++ LeetCode/202-happy-number/happy-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n/10>=1){
+            int sum=0;
+            while(n>0){
+                sum+=((n%10)*(n%10));
+                n/=10;
+                System.out.println(sum);
+            }
+            n=sum;
+        }
+        return n==1 || n==7;
+    }
+}
