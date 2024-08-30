5e4ea128444fb67147703b7a8dbbb027b75ed8f4
Nagaraja <98215614+nagaraja6050266@users.noreply.github.com>
2024-08-30 12:03:02 +0530

    Time: 42 ms (7.79%) | Memory: 45.5 MB (12.26%) - LeetSync

--- LeetCode/387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ LeetCode/387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -7,7 +7,7 @@ class Solution {
             else
                 map.put(s.charAt(i),1);
         }
-        System.out.println(map);
+        //System.out.println(map);
         for(int i=0;i<s.length();i++){
             if(map.get(s.charAt(i))==1){
                 return i;
