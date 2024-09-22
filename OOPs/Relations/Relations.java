9d1a7f917c1a665f1d746c475ccd9919cdcd1d12
nagaraja6050266 <nagaraja6050266@gmail.com>
2024-09-22 18:40:13 +0530

    Done upto attha ponnu

--- OOPs/Relations/Relations.java
+++ OOPs/Relations/Relations.java
@@ -13,29 +13,50 @@ class Member {
     public ArrayList<Member> siblings = new ArrayList<>();
     public Gender gender;
 
-    public ArrayList<Member> children = new ArrayList<>();
+    public ArrayList<Member> childrens = new ArrayList<>();
 
     public void setFather(Member father) {
         this.father = father;
-        father.children.add(this);
+        father.childrens.add(this);
     }
 
     public void setMother(Member mother) {
         this.mother = mother;
-        mother.children.add(this);
+        mother.childrens.add(this);
     }
 
-    Member(String name,Gender gender) {
+    public void setSibling(Member sibling) {
+        this.siblings.add(sibling);
+        sibling.siblings.add(this);
+    }
+
+    Member(String name, Gender gender) {
         this.name = name;
         this.gender = gender;
     }
 
-    public void setChildren(Member child){
-        this.children.add(child);
-        if(this.gender==Gender.MALE){
+    public void setChildren(Member child) {
+        this.childrens.add(child);
+        if (this.gender == Gender.MALE) {
             child.setFather(this);
         } else {
             child.setMother(this);
+        }
+    }
+
+    public void getAtthaPonnu() {
+        ArrayList<Member> aunties = new ArrayList<>();
+        for (Member member : this.father.siblings) {
+            if (member.gender == Gender.FEMALE) {
+                aunties.add(member);
+            }
+        }
+        for (Member aunty : aunties) {
+            for (Member child : aunty.childrens) {
+                if (child.gender == Gender.FEMALE) {
+                    System.out.println(child.name);
+                }
+            }
         }
     }
 
@@ -45,18 +66,20 @@ class Relations {
 
     public static void main(String[] args) {
         Member mani = new Member("Manikandan", Gender.MALE);
-        Member esakki = new Member("Esakki Muthu",Gender.MALE);
-        Member masanam = new Member("Masanam",Gender.MALE);
-        Member dummy = new Member("Suppaiya",Gender.MALE);
-        Member Manimother = new Member("Bala",Gender.MALE);
-
+        Member esakki = new Member("Esakki Muthu", Gender.MALE);
+        Member masanam = new Member("Masanam", Gender.MALE);
+        Member dummy = new Member("Suppaiya", Gender.MALE);
+        Member Manimother = new Member("Bala", Gender.MALE);
+        Member sandhiya = new Member("Sandhiya", Gender.FEMALE);
+        Member muthulakshmi = new Member("Muthulakshmi", Gender.FEMALE);
 
         mani.setFather(esakki);
         esakki.setFather(masanam);
         masanam.setFather(dummy);
+        sandhiya.setChildren(muthulakshmi);
+        esakki.setSibling(sandhiya);
 
-        System.out.println(mani.name);
-
+        mani.getAtthaPonnu();
 
     }
 
